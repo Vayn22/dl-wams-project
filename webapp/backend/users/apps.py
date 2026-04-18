08cb86a042c330c67e735464afc7f22fadcc4922
Islem Amine.MHD <mahiddineisam@gmail.com>
2026-04-18 21:00:10 +0100

    Added some NextJS and render stuff for testing 3

--- webapp/backend/users/apps.py
+++ webapp/backend/users/apps.py
@@ -3,8 +3,4 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     name = 'users'
 
-    def ready(self):
-        from django.contrib.auth.models import Group
-
-        Group.objects.get_or_create(name="Admin")
-        Group.objects.get_or_create(name="Doctor")
+    
